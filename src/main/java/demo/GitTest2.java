6911d0924be78ea7f5605e5972c09cb95509ce49
pengyy4 <pengyy4@lenovo.com>
2020-09-21 15:30:15 +0800

    git提交 测试pull conflict

--- src/main/java/demo/GitTest2.java
+++ src/main/java/demo/GitTest2.java
@@ -13,5 +13,6 @@ public class GitTest2 {
         System.out.println("git remote commit test pull");
         System.out.println("git commit test fetch");
         System.out.println("git remote commit test fetch");
+        System.out.println("git commit test pull conflict");
     }
 }
